a4e1995e00249ec083e5fdb7c004938e60aae385
datahaikuninja <jundai.takashina@gmail.com>
2024-05-11 17:46:03 +0900

    submit abc081b

--- abs/src/bin/abc081b.rs
+++ abs/src/bin/abc081b.rs
@@ -1,3 +1,26 @@
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        n: usize,
+        a: [i64; n],
+    }
+
+    let mut cnt = 1;
+    let base: i64 = 2;
+
+    loop {
+        let mut break_frag = false;
+        for i in &a {
+            if i % base.pow(cnt as u32) != 0 {
+                break_frag = true;
+            }
+        }
+
+        if break_frag {
+            break;
+        }
+
+        cnt += 1
+    }
+
+    println!("{}", cnt - 1);
 }
